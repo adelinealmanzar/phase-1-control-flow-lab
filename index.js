0a3ce4f867dae3e96c169653714325afe4ec3991
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-01-10 15:33:05 -0800

    commit tree 7640ccec49a4c814bd25efa66c4bac618471a5bc

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(num){
+  if (num <= 400) {
+    return 'This one is on me!';
+  } else if (num > 2000 && num < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (num > 2000 && num > 2500) {
+    return 'No can do.';
+  }
+};
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(name){
+  return name == 'NYC' ? ('Ok, sounds good.') : ('No go.');
+};
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+const phrase = 'Thank you so much';
+
+function switchOnCharmFromTip(phrase) {
+  switch(phrase) {
+    case  'generous':
+      return 'Thank you so much.';
+    break;
+    case 'not as generous':
+      return 'Thank you.';
+    break;
+    default:
+      return 'Bye.';
+    break;
+  }
+};
